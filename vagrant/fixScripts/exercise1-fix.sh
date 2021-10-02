3c4eef0a4ba741bfb6638a89bf9443361dfdcf89
Shir Sabach <Shirsab@gmail.com>
2021-10-02 16:36:47 +0300

    completed 1st excercise

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise1 here
+
+# what went wrong? The network was private
+# what was the fix? Making the network public by running the following command:
+Vagrant.configure("2") do |config|
+  config.vm.network "public_network"
+end
